791ee48eebeca922d3ba2ef5e1c39f5500dc5300
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-04-27 15:23:12 -0400

    removing debug statements

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/fortran/param_interfaces/convection/GF2020_interface.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/fortran/param_interfaces/convection/GF2020_interface.py
@@ -461,9 +461,7 @@ class GF2020Interface(UserCode):
                 )
 
             with TimedCUDAProfiler("GF 2020 Convection Numerics", {}):
-                self._managed_state.record("GF2020-In_python")
                 # adjust pbl_level from fortran indexing to python indexing
-                print("SHIFTING PBL LEVEL F TO P")
                 self._managed_state.ndsl_state.pbl_level.field[:] = self._managed_state.ndsl_state.pbl_level.field[:] - 1
 
                 # run GF 2020 Convection
@@ -472,10 +470,7 @@ class GF2020Interface(UserCode):
                     convection_tracers=self._managed_convection_tracers.ndsl_state,
                 )
                 # adjust pbl_level from python indexing to fortran indexing
-                print("SHIFTING PBL LEVEL P TO F")
                 self._managed_state.ndsl_state.pbl_level.field[:] = self._managed_state.ndsl_state.pbl_level.field[:] + 1
-
-                self._managed_state.record("GF2020-Out_python")
 
             with TimedCUDAProfiler("GF 2020 Convection - State copy-back", {}):
                 self._managed_state.ndsl_to_fortran()
